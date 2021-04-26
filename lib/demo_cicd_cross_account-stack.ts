ea104eafdf54719925b31f790bbb00706badb371
Nelli Lovchikova (nul) <nul@amazon.com>
2021-04-27 09:56:13 +1000

    Repo added

--- lib/demo_cicd_cross_account-stack.ts
+++ lib/demo_cicd_cross_account-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import { Tags } from '@aws-cdk/core';
+import { Repo } from './repo';
 
 export class DemoCicdCrossAccountStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const repo= new Repo(this, 'repo.construct');
+
+    Tags.of(this).add('purpose', 'demo');
+    Tags.of(this).add('app', 'cicd_cross_account');
   }
 }
---
+++ lib/repo.ts
@@ -0,0 +1,17 @@
+import { CfnOutput, Construct } from '@aws-cdk/core';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+
+export class Repo extends Construct {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+        var repository = new codecommit.Repository(this, 'demo.repo', {
+            repositoryName: 'demo01-repository',
+            description: 'demo01. Cross account CI/CD pipeline with CDK'
+        });
+
+        new CfnOutput(this, 'repo.git_url', {
+            value: repository.repositoryCloneUrlHttp,
+            description: "Demo repository http url"
+        });
+    }
+}
